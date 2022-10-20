a9ef34e9b3f5aeb7fc81749c2ead5ed6a4e4d9de
Sumedha2 <72296979+Sumedha2@users.noreply.github.com>
2022-10-20 22:41:39 +0530

    Added files

---
+++ DSA/Arrays/intro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // declaration
+    // 5 elements
+    int a[5];
+    // initialisation
+    // accessing elements
+    for (int i=0; i<5; i++){
+        a[i]=i;
+        cout<<a[i]<<" ";
+    }
+}
